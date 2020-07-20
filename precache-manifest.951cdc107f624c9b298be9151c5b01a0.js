2b693138952efbb0e50f07344cbea35505d8e001
aslan <social.smur@simplelogin.fr>
2020-07-20 15:05:53 +0100

    Updates

---
+++ precache-manifest.951cdc107f624c9b298be9151c5b01a0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e998ddb46929b040ba23367387456739",
+    "url": "/quoteGram-react/index.html"
+  },
+  {
+    "revision": "efd95434e5036b1c478f",
+    "url": "/quoteGram-react/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "68d1d8dbb4603a461df0",
+    "url": "/quoteGram-react/static/js/2.e9111860.chunk.js"
+  },
+  {
+    "revision": "74c0ddca854ac003128884c6311253d0",
+    "url": "/quoteGram-react/static/js/2.e9111860.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "efd95434e5036b1c478f",
+    "url": "/quoteGram-react/static/js/main.d3513afb.chunk.js"
+  },
+  {
+    "revision": "b4f660dcfc6cfc9f05cf",
+    "url": "/quoteGram-react/static/js/runtime-main.9a3ddba0.js"
+  }
+]);
